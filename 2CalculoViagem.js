d542a1c9803db696a6166c80d4fce6bdfd6dfd09
Stefane de Almeida <stefanedealmeida01@gmail.com>
2021-05-21 17:51:46 -0300

    Adicionei resolução simples sem entrada externa do exercicio 2

---
+++ 2CalculoViagem.js
@@ -0,0 +1,9 @@
+//2 - Calcule o tempo de uma viagem de carro.
+// Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let distancia = 600;
+let kmH = 80;
+
+let tempoMedia = distancia/kmH ;
+
+console.log(`Velocidade media = ${tempoMedia} horas`);
