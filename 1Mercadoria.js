37809ab94bc7b1f96833f3ac4677ccd83d5d2328
Stefane de Almeida <stefanedealmeida01@gmail.com>
2021-05-21 17:51:00 -0300

    Adicionei resolução simples sem entrada externa do exercicio 1

---
+++ 1Mercadoria.js
@@ -0,0 +1,8 @@
+//1 - Solicite o preço de uma mercadoria e o percentual de desconto 20%.
+//Exiba no console o valor do desconto e o preço a pagar.
+
+let mercadoria = 100
+const desconto = mercadoria * 0.2;
+let total = mercadoria - desconto;
+
+console.log(`O valor do desconto é R$ ${desconto} e o total a pagar R$ ${total}`)
