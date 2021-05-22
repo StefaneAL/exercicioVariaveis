eb573ed36642e4fb152cba762318cba5ff3486e7
Stefane de Almeida <stefanedealmeida01@gmail.com>
2021-05-21 21:15:06 -0300

    Adicionei resolução simples sem entrada externa do exercicio 7

---
+++ 7MaiorIgual.js
@@ -0,0 +1,11 @@
+//7 - Elabore um algoritmo que receba dois números e determine qual é o maior entre eles, 
+//se os números forem iguais, mostre uma mensagem no console "Os números são iguais".
+let num1 = 15, num2 = 15;
+
+if (num1 > num2 ){
+    console.log(`${num1} é maior que ${num2}`);
+}else if (num1 < num2){
+    console.log(`${num1} é menor que ${num2}`);
+}else {
+    console.log(`Os números são iguais`);
+}
