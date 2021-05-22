0de069551ef08d3cf27ad4e9c65f84dc59a254b0
Stefane de Almeida <stefanedealmeida01@gmail.com>
2021-05-21 21:02:18 -0300

    Adicionei resolução simples sem entrada externa do exercicio 6

---
+++ 6DiaSemana.js
@@ -0,0 +1,39 @@
+//6 - Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente.
+let diaEntrada = 6
+
+switch (diaEntrada){
+
+    case 1:
+        console.log(`Domingo`)
+        break
+
+
+    case 2:
+        console.log(`Segunda`)
+        break
+
+    case 3:
+        console.log(`Terça-Feira`)
+        break
+
+    case 4:
+        console.log(`Quarta-Feira`)
+        break
+
+    case 5:
+        console.log(`Quinta-Feira`)
+        break
+
+    case 6 :
+        console.log(`Sexta-Feira`)
+        break
+
+    case 7:
+        console.log(`Sabado`)
+        break
+
+    default:
+       console.log(`${diaEntrada} Não é um dia da semana `);
+       break
+}
+    
