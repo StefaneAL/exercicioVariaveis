20217ff9acaf8d496aff170bd6bade714abd4ea7
Stefane de Almeida <stefanedealmeida01@gmail.com>
2021-05-21 21:55:38 -0300

    Adicionei resolução simples sem entrada externa do exercicio 10

---
+++ 10Idade.js
@@ -0,0 +1,9 @@
+//10 - Crie uma variavel que recebe o ano de nascimento da pessoa informando se ela é maior de idade ou menor.
+
+let ano = 2003;
+
+if (ano <= 2003){
+    console.log(`Maior de idade`);
+}else {
+    console.log(`Menor de idade`);
+}
